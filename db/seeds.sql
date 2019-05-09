75bb5f5ca646b5add76edbdc10239e1dce3464c4
Dylan Wright <wright.dylan5@gmail.com>
2019-05-09 12:41:13 -0400

    added foundation

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers(burger_name, devoured)
+VALUES("Western", FALSE);
+
+INSERT INTO burgers(burger_name, devoured)
+VALUES("Cheese", FALSE);
+
+INSERT INTO burgers(burger_name, devoured)
+VALUES("Bacon", FALSE);
